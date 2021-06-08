00db1d01c7c2f0b6b09e14af6b25ccad9724d93a
shakaknight <ananddubey016@gmail.com>
2021-06-08 21:50:19 +0530

    First push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstFormComponent } from './app-form/first-form/first-form.component';
+import { SecondFormComponent } from './app-form/second-form/second-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '',redirectTo: 'first', pathMatch: 'full'},
+  {path: 'first', component: FirstFormComponent},
+  {path: 'second', component: SecondFormComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [FirstFormComponent,SecondFormComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from  '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { AppFormComponent } from './app-form/app-form.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppFormComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
